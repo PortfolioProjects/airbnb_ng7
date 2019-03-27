863a81b31924e683eaca8214cb4163346124c8c9
TanyaRan <tatyanaran@gmail.com>
2019-03-27 11:40:49 +0200

    Delete rental endpoint, manage user rentals, bookings endpoints

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const usersController = require('../controllers/users-controller');
 const bookingsController = require('../controllers/bookings-controller');
 
+router.get(
+  '/manage',
+  usersController.authMiddleware,
+  bookingsController.getUserBookings
+);
+
 router.post(
   '',
   usersController.authMiddleware,
